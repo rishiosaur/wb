a04b0a9b52e30fd74b2ad1ff21e634b43a0db508
Rishi Kothari <itsrishikothari@gmail.com>
2020-10-08 12:03:25 -0400

    rm stuff

--- config/server.js
+++ config/server.js
@@ -3,7 +3,7 @@ module.exports = ({ env }) => ({
   port: env.int('PORT', 1337),
   admin: {
     auth: {
-      secret: env('ADMIN_JWT_SECRET', '868d96cc27a73a5a76474ea4c3318a49'),
+      secret: env('ADMIN_JWT_SECRET'),
     },
   },
 });
